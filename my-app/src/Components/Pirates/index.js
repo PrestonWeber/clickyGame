a565e69b5a2544e2f11a2c337ab9059e68d37fa0
Preston <preston159@gmail.com>
2020-05-11 09:38:41 -0600

    images closer to being aligned the way I need them

--- my-app/src/Components/Pirates/index.js
+++ my-app/src/Components/Pirates/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Pirates(props) {
     return (
-        <div className="Container">
+        <div className="card">
         <div className="img-container">
             <img alt={props.name} src={props.image} />
             <div onClick={() => props.shuffle(props.id)}></div>
